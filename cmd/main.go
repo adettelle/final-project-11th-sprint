fed4d6cd8b0873d3c5cbcf9bd817eaa44c023752
Liudmila Litvintseva <l.litvinski@gmail.com>
2024-04-05 16:07:09 +0300

    initial implementation

---
+++ cmd/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/Yandex-Practicum/42-docker-final/pkg/parcels"
+
+	_ "modernc.org/sqlite"
+)
+
+func main() {
+	db, err := sql.Open("sqlite", "tracker.db")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	store := parcels.NewParcelStore(db)
+	service := parcels.NewParcelService(store)
+
+	// регистрация посылки
+	client := 1
+	address := "Псков, д. Пушкина, ул. Колотушкина, д. 5"
+	p, err := service.Register(client, address)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// изменение адреса
+	newAddress := "Саратов, д. Верхние Зори, ул. Козлова, д. 25"
+	err = service.ChangeAddress(p.Number, newAddress)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// изменение статуса
+	err = service.NextStatus(p.Number)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// вывод посылок клиента
+	err = service.PrintClientParcels(client)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// попытка удаления отправленной посылки
+	err = service.Delete(p.Number)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// вывод посылок клиента
+	// предыдущая посылка не должна удалиться, т.к. её статус НЕ «зарегистрирована»
+	err = service.PrintClientParcels(client)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// регистрация новой посылки
+	p, err = service.Register(client, address)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// удаление новой посылки
+	err = service.Delete(p.Number)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// вывод посылок клиента
+	// здесь не должно быть последней посылки, т.к. она должна была успешно удалиться
+	err = service.PrintClientParcels(client)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
